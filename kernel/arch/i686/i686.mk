5d17cb8cd6bad637c4aa2f4b44a05af8f4487742
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-01-14 23:32:15 -0500

    Nest sources into src directories.

--- kernel/arch/i686/i686.mk
+++ kernel/arch/i686/i686.mk
@@ -1,31 +1,32 @@
 ARCH_DIR = kernel/arch/i686
+ARCH_SOURCE_DIR = $(ARCH_DIR)/src
+ARCH_INCLUDE_DIR = $(ARCH_DIR)/include
 ARCH_BUILD_DIR = build/kernel/arch
-ARCH_INCLUDE = $(ARCH_DIR)/include
 
-ARCH_ASMSOURCES := $(wildcard $(ARCH_DIR)/*.asm)
-ARCH_ASMOBJECTS := $(patsubst $(ARCH_DIR)/%.asm,$(ARCH_BUILD_DIR)/%.o,$(ARCH_ASMSOURCES))
+ARCH_ASMSOURCES := $(wildcard $(ARCH_SOURCE_DIR)/*.asm)
+ARCH_ASMOBJECTS := $(addprefix $(ARCH_BUILD_DIR)/, $(notdir $(patsubst %.asm, %.o, $(ARCH_ASMSOURCES))))
 
-ARCH_TEMPLATES := $(wildcard $(ARCH_DIR)/*.c.sh)
-ARCH_TEMPLATE_SOURCES := $(patsubst $(ARCH_DIR)/%.c.sh,$(ARCH_BUILD_DIR)/%.c,$(ARCH_TEMPLATES))
+ARCH_TEMPLATES := $(wildcard $(ARCH_SOURCE_DIR)/*.c.sh)
+ARCH_TEMPLATE_SOURCES := $(addprefix $(ARCH_BUILD_DIR)/, $(notdir $(patsubst %.c.sh, %.c, $(ARCH_TEMPLATES))))
 
-ARCH_CSOURCES := $(wildcard $(ARCH_DIR)/*.c)
-ARCH_COBJECTS := $(patsubst $(ARCH_DIR)/%.c,$(ARCH_BUILD_DIR)/%.o,$(ARCH_CSOURCES)) \
-			     $(patsubst $(ARCH_BUILD_DIR)/%.c,$(ARCH_BUILD_DIR)/%.o,$(ARCH_TEMPLATE_SOURCES))
-ARCH_CDEPS := $(patsubst $(ARCH_BUILD_DIR)/%.o,$(ARCH_BUILD_DIR)/%.d,$(ARCH_COBJECTS))
+ARCH_CSOURCES := $(wildcard $(ARCH_SOURCE_DIR)/*.c)
+ARCH_COBJECTS := $(addprefix $(ARCH_BUILD_DIR)/, $(notdir $(patsubst %.c, %.o, $(ARCH_CSOURCES)))) \
+			     $(addprefix $(ARCH_BUILD_DIR)/, $(notdir $(patsubst %.c, %.o, $(ARCH_TEMPLATE_SOURCES))))
+ARCH_CDEPS := $(patsubst %.o, %.d, $(ARCH_COBJECTS))
 
 ARCH_OBJECTS = $(ARCH_ASMOBJECTS) $(ARCH_COBJECTS)
 
 ASFLAGS := $(ASFLAGS) -felf32
-CFLAGS := $(CFLAGS) -I $(ARCH_INCLUDE) --target=i686-pc-none-elf -march=i686
-LDFLAGS := $(LDFLAGS) -T $(ARCH_DIR)/linker.ld --target=i686-pc-none-elf -march=i686
+CFLAGS := $(CFLAGS) -I $(ARCH_INCLUDE_DIR) --target=i686-pc-none-elf -march=i686
+LDFLAGS := $(LDFLAGS) -T $(ARCH_SOURCE_DIR)/linker.ld --target=i686-pc-none-elf -march=i686
 
-$(ARCH_BUILD_DIR)/%.o: $(ARCH_DIR)/%.asm
+$(ARCH_BUILD_DIR)/%.o: $(ARCH_SOURCE_DIR)/%.asm
 	$(AS) $(ASFLAGS) -o $@ $<
 
-$(ARCH_BUILD_DIR)/%.o: $(ARCH_DIR)/%.c
+$(ARCH_BUILD_DIR)/%.o: $(ARCH_SOURCE_DIR)/%.c
 	$(CC) $(CFLAGS) -MMD -MP -c $< -o $@
 
-$(ARCH_BUILD_DIR)/%.c: $(ARCH_DIR)/%.c.sh
+$(ARCH_BUILD_DIR)/%.c: $(ARCH_SOURCE_DIR)/%.c.sh
 	bash -c $< > $@
 
 -include $(ARCH_CDEPS)
--- kernel/kernel.mk
+++ kernel/kernel.mk
@@ -1,15 +1,16 @@
 KERNEL_DIR = kernel
+KERNEL_SOURCE_DIR = $(KERNEL_DIR)/src
+KERNEL_INCLUDE_DIR = $(KERNEL_DIR)/include
 KERNEL_BUILD_DIR = build/kernel
-KERNEL_INCLUDE = $(KERNEL_DIR)/include
 
-KERNEL_SOURCES := $(wildcard $(KERNEL_DIR)/*.c)
+KERNEL_SOURCES := $(wildcard $(KERNEL_SOURCE_DIR)/*.c)
 KERNEL_OBJECTS := $(addprefix $(KERNEL_BUILD_DIR)/, $(notdir $(patsubst %.c,%.o,$(KERNEL_SOURCES))))
 
 include kernel/arch/i686/i686.mk
 
-CFLAGS := $(CFLAGS) -I $(KERNEL_INCLUDE)
+CFLAGS := $(CFLAGS) -I $(KERNEL_INCLUDE_DIR)
 
 OBJECTS := $(OBJECTS) $(ARCH_OBJECTS) $(KERNEL_OBJECTS)
 
-$(KERNEL_BUILD_DIR)/%.o: $(KERNEL_DIR)/%.c
+$(KERNEL_BUILD_DIR)/%.o: $(KERNEL_SOURCE_DIR)/%.c
 	$(CC) $(CFLAGS) -MMD -MP -c $< -o $@
