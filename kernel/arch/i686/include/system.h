d1dab9064e36f5fcc5ea7b8f84dcb1864a4dc1fe
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-01-20 21:26:00 -0500

    Add MSR helpers.

--- kernel/arch/i686/include/system.h
+++ kernel/arch/i686/include/system.h
@@ -6,4 +6,14 @@
 typedef uint32_t paddr_t;
 typedef uint32_t vaddr_t;
 
+static inline void get_msr(uint32_t msr, uint32_t* low, uint32_t* high)
+{
+    __asm__ __volatile__("rdmsr" : "=a"(*low), "=d"(*high) : "c"(msr));
+}
+
+static inline void set_msr(uint32_t msr, uint32_t low, uint32_t high)
+{
+    __asm__ __volatile__("wrmsr" : : "a"(low), "d"(high), "c"(msr));
+}
+
 #endif
